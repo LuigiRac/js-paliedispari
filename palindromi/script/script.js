c9502588da5b0b98f3aa637f544891c49bc4e44b
LuigiRac <lui.rac06@gmail.com>
2024-11-06 10:07:08 +0100

    Palindroma: insert word

---
+++ palindromi/script/script.js
@@ -0,0 +1,11 @@
+"use strict"
+console.clear()
+
+/* 
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma 
+*/
+
+let userWord = prompt("Inserire una parola, grazie!")
+  console.log ("La parola inserita è: " + userWord + "!")
