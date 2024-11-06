03f3efec2223b58b5cdb601fe4a016b8d3c5d3e6
LuigiRac <lui.rac06@gmail.com>
2024-11-06 16:52:39 +0100

    Code - Formatted Palindromi

--- palindromi/script/script.js
+++ palindromi/script/script.js
@@ -8,30 +8,27 @@ Creare una funzione per capire se la parola inserita è palindroma
 */
 
 const userWord = prompt("Inserire una parola, grazie!")
-  console.log ("La parola inserita è: " + userWord + "!")
+console.log("La parola inserita è: " + userWord + "!")
 
 
-  const reversedText = turnWord(userWord);
+const reversedText = turnWord(userWord);
 
-  if (userWord.toLowerCase() === reversedText.toLowerCase()){
-    console.log("La parola è palindroma");
-  }
-  else {
-    console.log("La parola NON è palindroma")
-  }
-  console.log(reversedText);
-  
+if (userWord.toLowerCase() === reversedText.toLowerCase()) {
+  console.log("La parola è palindroma");
+}
+else {
+  console.log("La parola NON è palindroma")
+}
+console.log(reversedText);
 
-  function turnWord(word) {
-    let reversedWord = ""; 
 
-    for (let i = word.length -1; i >= 0; i--){
-      reversedWord = reversedWord + userWord[i];
-    }
+function turnWord(word) {
+  let reversedWord = "";
 
-    return reversedWord;
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversedWord = reversedWord + userWord[i];
   }
 
+  return reversedWord;
+}
 
-
-  
